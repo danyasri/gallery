62a6c2adbbefc892b3d942ec798e43d76d32c87f
Danya Sri Anantha prakash <danyasri@Danyas-MacBook-Pro.local>
2020-08-16 21:40:06 -0700

    my Gallery

--- src/App.js
+++ src/App.js
@@ -2,23 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const files = ['img1.JPG', 'img2.jpg', 'img3.png', 'img7.png', 'img11.png', 'img12.png', 'img5.png', 'img6.png', 'img3.jpg', 'img4.png', 'img8.png', 'img9.png', 'img10.png', 'img13.png', 'img14.jpg', 'img15.png'];
+const Image = ({src}) => <img src={`images/${src}`}></img>;
+const Gallery = ({images}) => images.map((src) => <Image src={src} />);
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Gallery images={files} />
     </div>
   );
 }
