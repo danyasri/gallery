3ce56b51b4ae68e3f89e692c0a3b1b89a7e9ff81
Danya Sri Anantha prakash <danyasri@Danyas-MacBook-Pro.local>
2020-08-16 21:58:21 -0700

    Updates

---
+++ precache-manifest.cfff11805379a14bed49d1c3b7da4e58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31aa31981fa0cef424e4c1cbf04e8a9",
+    "url": "/gallery/index.html"
+  },
+  {
+    "revision": "dbe854f23915dd616cfc",
+    "url": "/gallery/static/css/main.d330ed6f.chunk.css"
+  },
+  {
+    "revision": "f17eaec48e564cf09bb7",
+    "url": "/gallery/static/js/2.c6f147ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gallery/static/js/2.c6f147ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe854f23915dd616cfc",
+    "url": "/gallery/static/js/main.626c8c48.chunk.js"
+  },
+  {
+    "revision": "1160e6796d671babbaaa",
+    "url": "/gallery/static/js/runtime-main.7b22ee28.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gallery/static/media/logo.5d5d9eef.svg"
+  }
+]);
